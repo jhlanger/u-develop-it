b43a3f2ef2b5db4b3f6258386c27b39de046564b
Jonhe <Jonhenrylangerman@gmail.com>
2021-08-26 19:19:14 -0700

    12.4

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+
+
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'Madison99!',
+  database: 'election'
+});
+
+
+module.exports = db;
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,39 @@
+/* mysql -u root -p */
+
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
+
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+/* query to join party values into candidates
+SELECT candidates.*, parties.name
+FROM candidates
+LEFT JOIN parties ON candidates.party_id = parties.id;
+
+
+SELECT candidates.*, parties.name AS party_name
+FROM candidates
+LEFT JOIN parties ON candidates.party_id = parties.id;
+*/
